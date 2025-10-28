1a9be5aa4ec085ede375ec9600f9ce7cad46295e
manNomi <hanmw110@naver.com>
2025-10-28 22:19:16 +0900

    init: 기본 상수,view 구현

---
+++ src/constants/message.js
@@ -0,0 +1,16 @@
+export const ERROR_MESSAGE = {
+  NOT_NUMBER: '[ERROR] 숫자가 아닌 값이 포함되었습니다.',
+  NOT_1000_UNIT: '[ERROR] 1000원 단위가 아닙니다.',
+  NOT_POSITIVE: '[ERROR] 0원 이하인 경우',
+  NOT_6_LENGTH: '[ERROR] 6개가 아닌 경우',
+  NOT_1_45_RANGE: '[ERROR] 1~45 범위를 벗어난 숫자가 있는 경우',
+  NOT_SPACE: '[ERROR] 공백이 포함되는 경우 (EX: 1, ,3)',
+  NOT_DUPLICATE: '[ERROR] 중복된 숫자가 있는 경우',
+  NOT_DUPLICATE_WITH_WINNING_NUMBER: '[ERROR] 당첨번호와 증복입니다',
+};
+
+export const INPUT_MESSAGE = {
+  ASK_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.',
+  ASK_WINNING_NUMBER: '당첨 번호를 입력해 주세요.',
+  ASK_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+};
