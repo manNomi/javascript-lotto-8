b8cbc8b95253d3fd5196813f67126e56ed9ac12a
manNomi <hanmw110@naver.com>
2025-10-28 22:38:02 +0900

    test: Lotto 테스트 코드 작성

--- src/model/Lotto.test.js
+++ src/model/Lotto.test.js
@@ -14,5 +14,28 @@ describe('로또 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+  test.each([
+    [0, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 46],
+  ])('로또 번호가 1에서 45 범위를 벗어나면 예외가 발생한다.', (...numbers) => {
+    expect(() => {
+      new Lotto(numbers);
+    }).toThrow('[ERROR]');
+  });
+
+  test.each([
+    [1, 2, 3, NaN, 5, 6],
+    [1, 2, 3, 'a', 5, 6],
+    [1, 2, 3, '@', 5, 6],
+  ])('로또 번호에 숫자가 아닌 값이 포함되면 예외가 발생한다.', (...numbers) => {
+    expect(() => {
+      new Lotto(numbers);
+    }).toThrow('[ERROR]');
+  });
+
+  test('유효한 로또 번호가 입력되면 예외가 발생하지 않는다.', () => {
+    expect(() => {
+      new Lotto([1, 2, 3, 4, 5, 6]);
+    }).not.toThrow('[ERROR]');
+  });
 });
