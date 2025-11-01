077010ef1d3efcc0ec91e52bbf42cc27fb08a1eb
manNomi <hanmw110@naver.com>
2025-11-01 22:42:20 +0900

    feat: Lotto 코드 반환 함수 수정

--- src/service/LottoGame.js
+++ src/service/LottoGame.js
@@ -48,14 +48,14 @@ export default class LottoGame {
   }
 
   #countMatchResults(matchResults) {
-    const counts = { first: 0, second: 0, third: 0, fourth: 0, fifth: 0 };
+    const counts = { fifth: 0, fourth: 0, third: 0, second: 0, first: 0 };
 
     matchResults.forEach(({ matchCount, hasBonus }) => {
       if (matchCount === 6) counts.first += 1;
       else if (matchCount === 5 && hasBonus) counts.second += 1;
-      else if (matchCount === 5) counts.fifth += 1;
+      else if (matchCount === 5) counts.third += 1;
       else if (matchCount === 4) counts.fourth += 1;
-      else if (matchCount === 3) counts.third += 1;
+      else if (matchCount === 3) counts.fifth += 1;
     });
 
     return counts;
@@ -63,37 +63,11 @@ export default class LottoGame {
 
   #buildStatistics(counts) {
     return [
-      {
-        match: 3,
-        prize: 5000,
-        count: counts.third,
-        message: '3개 일치 (5,000원)',
-      },
-      {
-        match: 4,
-        prize: 50000,
-        count: counts.fourth,
-        message: '4개 일치 (50,000원)',
-      },
-      {
-        match: 5,
-        prize: 1500000,
-        count: counts.fifth,
-        message: '5개 일치 (1,500,000원)',
-      },
-      {
-        match: 5,
-        bonus: true,
-        prize: 30000000,
-        count: counts.second,
-        message: '5개 일치, 보너스 볼 일치 (30,000,000원)',
-      },
-      {
-        match: 6,
-        prize: 2000000000,
-        count: counts.first,
-        message: '6개 일치 (2,000,000,000원)',
-      },
+      { match: 3, prize: 5000, count: counts.fifth },
+      { match: 4, prize: 50000, count: counts.fourth },
+      { match: 5, prize: 1500000, count: counts.third },
+      { match: 5, prize: 30000000, count: counts.second, bonus: true },
+      { match: 6, prize: 2000000000, count: counts.first },
     ];
   }
 
